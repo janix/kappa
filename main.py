60d2b1cb62558c94936d3895078349643e31d81e
Janusz Janik <janusz.janik@hotmail.com>
2017-12-18 22:52:16 +0100

    kappa

---
+++ main.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 18 22:00:29 2017
+
+@author: janus
+"""
+
+def is_prime(n):
+  if n == 2 or n == 3: return True
+  if n < 2 or n%2 == 0: return False
+  if n < 9: return True
+  if n%3 == 0: return False
+  r = int(n**0.5)
+  f = 5
+  while f <= r:
+#   print ('\t',f)
+    if n%f == 0: return False
+    if n%(f+2) == 0: return False
+    f +=6
+  return True
+
+array_primes = []
+array_diff = []
+
+previous=0
+for i in range(1000):
+    if(is_prime(i)):
+        diff = i - previous
+        previous = i
+        array_primes.append(i)
+        array_diff.append(diff)
+
+print(array_primes)
+print(array_diff)    
