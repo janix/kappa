511a408ccb3162cb702aabe48f1d501dcb645faf
janix <janusz.janik@hotmail.com>
2019-05-04 03:55:15 +0200

    work

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 def is_prime(n):
   if n == 2 or n == 3: return True
   if n < 2 or n%2 == 0: return False
@@ -6,7 +8,6 @@ def is_prime(n):
   r = int(n**0.5)
   f = 5
   while f <= r:
-#   print ('\t',f)
     if n%f == 0: return False
     if n%(f+2) == 0: return False
     f +=6
@@ -20,9 +21,24 @@ def suma(g):
         sum += d
     return sum
 
-kappa = 100
-for i in range(kappa):
-    if is_prime(i):
-        print i, "      ",suma(suma(i))
+number_limit = 1000
+vector = []
 
-        
+for i in range(4,number_limit,1):
+    if(is_prime(i)):
+        j = suma(suma(suma(i)))
+        vector.append([i,j])
+
+#for n in range(1,9,1):
+#    for item in vector:
+#        if item[1] == n:
+#            print item
+
+index = 0
+for item in vector:
+    index += 1
+    print (item[1]),
+    if index % 6 == 0:
+        print(" ")
+
+#print(vector)
